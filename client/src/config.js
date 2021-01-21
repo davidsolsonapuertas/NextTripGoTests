13e4d1bd6c4f2a6edb3cb5fe10303f738dc88e97
Caroline Victor-Pujebet <caroline_pujebet@hotmail.com>
2021-01-21 16:22:39 +0000

    add google api key

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  GoogleKey: '',
+  GoogleKey: 'AIzaSyDJV7v2woL4PiKQNWITGT-rdbGw69dW1rY',
   PexelKey: '563492ad6f91700001000001e4172f2f8b8d4ea69fe866084ca9ceb0',
   CurrencyKey: '0d3dacbd653735cd1102'
 };
